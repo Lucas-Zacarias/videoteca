1c45f1b5977bf3c3130d20c79db646929c5f6fe8
Lucas Zacarias <lucas21111999@gmail.com>
2021-06-17 14:40:28 -0300

    agregué para usar binding y obtuve las views

--- app/src/main/java/com/unlam/edu/ar/videotecamoviltp/LogInActivity.kt
+++ app/src/main/java/com/unlam/edu/ar/videotecamoviltp/LogInActivity.kt
@@ -2,10 +2,31 @@ package com.unlam.edu.ar.videotecamoviltp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import com.unlam.edu.ar.videotecamoviltp.databinding.ActivityLogInBinding
 
 class LogInActivity : AppCompatActivity() {
+
+    private lateinit var email : EditText
+    private lateinit var password : EditText
+    private lateinit var btnLogIn : Button
+    private lateinit var btnSignUp : Button
+    private lateinit var binding: ActivityLogInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        getViews()
     }
+
+    private fun getViews() {
+        email = binding.emailEditText
+        password = binding.passwordEditText
+        btnLogIn = binding.logInButton
+        btnSignUp = binding.signUpButton
+    }
+    
 }
