e14dfa286101f1d302a124dd1f8ef9a44899b06e
Lucas Zacarias <lucas21111999@gmail.com>
2021-06-29 14:34:00 -0300

    se empezo a usar binding en favactivity

--- app/src/main/java/com/unlam/edu/ar/videotecamoviltp/FavActivity.kt
+++ app/src/main/java/com/unlam/edu/ar/videotecamoviltp/FavActivity.kt
@@ -2,10 +2,14 @@ package com.unlam.edu.ar.videotecamoviltp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.unlam.edu.ar.videotecamoviltp.databinding.ActivityFavBinding
 
 class FavActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFavBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fav)
+        binding = ActivityFavBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
     }
 }
