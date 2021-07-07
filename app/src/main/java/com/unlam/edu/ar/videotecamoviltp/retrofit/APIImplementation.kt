9ae05b354d4d68c36162f172c8e1accc85a19cc2
AgustinGug <agustintvg23@hotmail.com>
2021-07-06 21:36:30 -0300

    modificado

--- app/src/main/java/com/unlam/edu/ar/videotecamoviltp/retrofit/APIImplementation.kt
+++ app/src/main/java/com/unlam/edu/ar/videotecamoviltp/retrofit/APIImplementation.kt
@@ -2,11 +2,9 @@ package com.unlam.edu.ar.videotecamoviltp.retrofit
 
 import com.unlam.edu.ar.videotecamoviltp.model.Genres
 import com.unlam.edu.ar.videotecamoviltp.model.MovieFav_Details_Model
+import com.unlam.edu.ar.videotecamoviltp.model.MovieGenre
 import com.unlam.edu.ar.videotecamoviltp.model.Movies
-<<<<<<< HEAD:app/src/main/java/com/unlam/edu/ar/videotecamoviltp/retrofit/APIImplementation.kt
-=======
 import retrofit2.Call
->>>>>>> 7115ab07dc21a980918150ab166ff41472c37325:app/src/main/java/com/unlam/edu/ar/videotecamoviltp/service/API.kt
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -25,17 +23,22 @@ class APIImplementation : RetrofitApiService {
         .build()
         .create(ApiInterface::class.java)
 
-
+    private val serviceMovie: ApiInterface = Retrofit.Builder()
+        .baseUrl("https://api.themoviedb.org/3/movie/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiInterface::class.java
+        )
 
     override fun getMovie(title: String, callback: Callback<Movies>) {
         serviceSearch.getMovie(title).enqueue(callback)
     }
 
-<<<<<<< HEAD:app/src/main/java/com/unlam/edu/ar/videotecamoviltp/retrofit/APIImplementation.kt
     override fun getGenreID(title: Int, callback: Callback<Genres>) {
         serviceDiscover.getGenreID(title).enqueue(callback)
     }
-=======
 
->>>>>>> 7115ab07dc21a980918150ab166ff41472c37325:app/src/main/java/com/unlam/edu/ar/videotecamoviltp/service/API.kt
+    override fun getMovieID(title: Int, callback: Callback<MovieGenre>){
+        serviceMovie.getMovieID(title).enqueue(callback)
+    }
 }
